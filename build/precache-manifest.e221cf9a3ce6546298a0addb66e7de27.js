34a45d22214fc85a5104046a1c273c5e5f4ef924
JunBro1908 <hyeang1202@naver.com>
2023-02-01 15:55:30 +0900

    ganache local network update

---
+++ build/precache-manifest.e221cf9a3ce6546298a0addb66e7de27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e44e58f63c50f38c047",
+    "url": "/defi-staking-web/static/css/main.a94446bb.chunk.css"
+  },
+  {
+    "revision": "4e44e58f63c50f38c047",
+    "url": "/defi-staking-web/static/js/main.4e44e58f.chunk.js"
+  },
+  {
+    "revision": "da11cc6be1453b14804d",
+    "url": "/defi-staking-web/static/css/1.7d70e058.chunk.css"
+  },
+  {
+    "revision": "da11cc6be1453b14804d",
+    "url": "/defi-staking-web/static/js/1.da11cc6b.chunk.js"
+  },
+  {
+    "revision": "1afd7e71dfb52c5bcfc5",
+    "url": "/defi-staking-web/static/js/runtime~main.1afd7e71.js"
+  },
+  {
+    "revision": "589bd4ec5e634342e2f1c0dbef6ca9ad",
+    "url": "/defi-staking-web/static/media/bank.589bd4ec.png"
+  },
+  {
+    "revision": "7c1a10278ebc358859586c76a3e43a0b",
+    "url": "/defi-staking-web/static/media/mbr.7c1a1027.png"
+  },
+  {
+    "revision": "c498cac045a0b02c0d856787e98d08ee",
+    "url": "/defi-staking-web/static/media/MBR_poster.c498cac0.png"
+  },
+  {
+    "revision": "17a2e989f90b790c1016909a7c1e6826",
+    "url": "/defi-staking-web/index.html"
+  }
+];
